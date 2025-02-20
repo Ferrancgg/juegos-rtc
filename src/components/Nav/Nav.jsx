49deb398e150bc619294ceae45b54eed2e8d061c
TuNombreDeUsuario <ferrancgg@gmail.com>
2025-02-20 23:24:00 +0100

    ultimos retoques CSS

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import "./Nav.css"
 const Nav = () => {
   return (
     <nav className="nav">
-        <NavLink className="nav-link" to="/tres_en_raya">Tres en Raya</NavLink>
+        <NavLink className="nav-link" to="/tres_en_raya">Tres Raya</NavLink>
         <NavLink className="nav-link" to="/sudoku">Sudoku</NavLink>
         <NavLink className="nav-link" to="/ahorcado">Ahorcado</NavLink>
         
